e73c4752cd1d5523e2bae4baa2ca16fecc5f1657
neverlose <50455388+BuiBaAnh@users.noreply.github.com>
2024-01-12 16:39:55 +0700

    Update decomposition_net_train.py

--- decomposition_net_train.py
+++ decomposition_net_train.py
@@ -81,9 +81,9 @@ print("[*] Initialize model successfully...")
 ###train_data
 train_low_data = []
 train_high_data = []
-train_low_data_names = glob('./LOLdataset/our485/low/*.png') 
+train_low_data_names = glob('/kaggle/input/seperate-dataset-for-right-format/puwell/train/low/*.png') 
 train_low_data_names.sort()
-train_high_data_names = glob('./LOLdataset/our485/high/*.png') 
+train_high_data_names = glob('/kaggle/input/seperate-dataset-for-right-format/puwell/train/high/*.png') 
 train_high_data_names.sort()
 assert len(train_low_data_names) == len(train_high_data_names)
 print('[*] Number of training data: %d' % len(train_low_data_names))
@@ -95,9 +95,9 @@ for idx in range(len(train_low_data_names)):
 ###eval_data
 eval_low_data = []
 eval_high_data = []
-eval_low_data_name = glob('./LOLdataset/eval15/low/*.png')
+eval_low_data_name = glob('/kaggle/input/seperate-dataset-for-right-format/puwell/test/low/*.png')
 eval_low_data_name.sort()
-eval_high_data_name = glob('./LOLdataset/eval15/high/*.png*')
+eval_high_data_name = glob('/kaggle/input/seperate-dataset-for-right-format/puwell/test/high/*.png')
 eval_high_data_name.sort()
 for idx in range(len(eval_low_data_name)):
     eval_low_im = load_images(eval_low_data_name[idx])
